53c82ae237da3fc93002cf0a13624d767d703771
arpine-kirakosyan <arpinekirakosyan@gmail.com>
2020-11-24 16:01:25 +0400

    Counter, Reset Button, Disable the button, if the count is 0

--- src/counter/Counter.jsx
+++ src/counter/Counter.jsx
@@ -4,34 +4,43 @@ import './counter.css';
 class Counter extends Component {
     constructor() {
         super();
-        this.handleClickDecrease = this.handleClickDecrease.bind(this);
-        this.handleClickIncrease = this.handleClickIncrease.bind(this);
+        this.calc = this.calc.bind(this);
+        this.reset = this.reset.bind(this);
     }
 
     state = {
         counter: 0,
     }    
-    //Decrease count
-    handleClickDecrease() {
+    calc(type) {
+        if(type=="Decrease"){
+            this.state.counter--;         
+         }
+        else if(type=="Increase"){
+            this.state.counter++;
+        }
+
         this.setState({
-            count: this.state.counter++
+            count: this.state.counter
         })
     }
-     //Increase count
-    handleClickIncrease() {
+    reset(){        
+
         this.setState({
-            count: this.state.counter--
-        })
+             count:this.state.counter = 0
+        })      
+              
+
     }
 
     render() {
         return (
             <div className="counter-box">
                 <h2>Counter</h2>
+                <button onClick={this.reset} className="reset-btn">Reset</button>
                 <div className="counter-box__content">
-                    <button onClick={this.handleClickDecrease}>Decrease</button>
+                    <button onClick={()=>this.calc("Decrease")} className={this.state.counter== 0 ? ("decrease-btn disabled"):"decrease-btn" }>-</button>
                     <p>{this.state.counter}</p>
-                    <button onClick={this.handleClickIncrease}>Increase</button>
+                    <button onClick={()=>this.calc("Increase")} className="increase-btn">+</button>
                 </div>
             </div>
         )
